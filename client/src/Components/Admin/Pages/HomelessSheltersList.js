8463eaabe2692c1b629b89908aa75bd8a33d26e8
Richard Shu <richardjshu@gmail.com>
2018-09-15 17:56:13 -0400

    Add homeless shelter boxes

--- client/src/Components/Admin/Pages/HomelessSheltersList.js
+++ client/src/Components/Admin/Pages/HomelessSheltersList.js
@@ -1,4 +1,13 @@
 import React, { Component } from 'react';
+
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import { CardContent } from '@material-ui/core';
+import CardActions from '@material-ui/core/CardActions';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
 import './HomelessSheltersList.css';
 
 class HomelessSheltersList extends Component {
@@ -12,20 +21,34 @@ class HomelessSheltersList extends Component {
     componentDidMount() {
         fetch('api/homelessShelters')
             .then(res => res.json())
-            .then(homelessShelters => this.setState({homelessShelters}, () => 
-            console.log('Homeless shelters fetched...', homelessShelters)
-        ));
+            .then(homelessShelters => this.setState({ homelessShelters }));
     }
 
     render() {
         return (
             <div>
                 <h1>Your homeless shelters</h1>
-                <ul>
+                <Grid container spacing={40}>
+                    <Grid item xs={2}></Grid>
                     {this.state.homelessShelters.map(homelessShelter =>
-                        <li key={homelessShelter.id}> { homelessShelter.name } { homelessShelter.address } </li> 
+                        <Grid item xs key={homelessShelter.id}>
+                            <Card>
+                                <CardHeader title={homelessShelter.name} />
+                                <CardContent>
+                                    <Typography>{homelessShelter.address}</Typography>
+                                    <Typography>{homelessShelter.phoneNumber}</Typography>
+                                    <Typography>{homelessShelter.numberOfBeds}</Typography>
+                                    <Typography>{homelessShelter.numberOfAvailableBeds}</Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button>Edit</Button>
+                                    <Button>Delete</Button>
+                                </CardActions>
+                            </Card>
+                        </Grid>     
                     )}
-                </ul>
+                    <Grid item xs={2}></Grid>
+                </Grid>
             </div>
         );
     }
--- server.js
+++ server.js
@@ -6,28 +6,31 @@ app.get('/api/homelessShelters', (req, res) => {
     //name, address, phone, number of beds, free beds
     const homelessShelters = [
         {
-            id: 1, 
+            id: 1,
             name: 'Homeless Shelter 1', 
             address: 'Address 1', 
-            phone: '111-111-1111',
+            phoneNumber: '111-111-1111',
             numberOfBeds: 100,
-            numberOfFreeBeds: 23
+            numberOfAvailableBeds: 23,
+            websiteURL: 'https://google.com'
         },
         {
-            id: 2, 
+            id: 2,
             name: 'Homeless Shelter 2', 
             address: 'Address 2',
-            phone: '222-222-2222',
+            phoneNumber: '222-222-2222',
             numberOfBeds: 75,
-            numberOfFreeBeds: 17
+            numberOfAvailableBeds: 17,
+            websiteURL: 'https://bing.com'
         },
         {
-            id: 3, 
+            id: 3,
             name: 'Homeless Shelter 3', 
             address: 'Address 3',
-            phone: '333-333-3333',
+            phoneNumber: '333-333-3333',
             numberOfBeds: 50,
-            numberOfFreeBeds: 32
+            numberOfAvailableBeds: 32,
+            websiteURL: 'https://yahoo.com'
         }
     ];
     res.json(homelessShelters);
