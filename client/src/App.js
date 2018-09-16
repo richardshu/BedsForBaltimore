a11ec2ce4e0dba9ed9f41143888fa0a0683a9ed0
sewardhorace <maxjwhite22@gmail.com>
2018-09-16 02:43:02 -0400

    connecting to TestData collection

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
+import { Stitch, RemoteMongoClient, AnonymousCredential } from 'mongodb-stitch-browser-sdk'
+
 import SheltersList from './Components/User/Pages/SheltersList';
 import Navbar from './Components/Navbar/Navbar';
 import './App.css';
+
+const client = Stitch.initializeDefaultAppClient('homelesshaven-tcmuc');
+const collection = client.getServiceClient(RemoteMongoClient.factory, 'mongodb-atlas').db('Shelters').collection("TestData");
 
 class App extends Component {
 
@@ -13,17 +18,26 @@ class App extends Component {
   }
 
   componentDidMount(){
-    //database request here
-    this.setState({
-      homelessShelters : [
-        {
-          name: "Dummy",
-          address: "123 Main",
-          phone: "123-123-1234",
-          website: "example.com",
-          freeBeds: 10
-        }
-      ]
+    if (Stitch.defaultAppClient.auth.isLoggedIn) {
+      this.getShelterData();
+    } else {
+      Stitch.defaultAppClient.auth.loginWithCredential(new AnonymousCredential()).then(user => {
+        console.log(`Logged in as anonymous user with id: ${user.id}`);
+        this.getShelterData();
+      }).catch(console.error);
+    }
+  }
+
+  getShelterData(){
+    const query = {};
+    const params = {sort: {"freeBeds" : -1 }};
+    collection.find(query, params).asArray().then(docs => {
+      const newState = {
+        homelessShelters : docs,
+        collection : collection
+      };
+      this.setState(newState);
+      console.log(docs);
     });
   }
 
