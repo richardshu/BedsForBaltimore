d3c93728412c92063e0b1656de0330b3530d4dca
sewardhorace <maxjwhite22@gmail.com>
2018-09-16 01:28:44 -0400

    removed unused styles, readability adjustments

--- client/src/Components/User/Pages/Shelter.js
+++ client/src/Components/User/Pages/Shelter.js
@@ -10,17 +10,16 @@ class Shelter extends Component {
         const shelter = this.props.shelter;
         const buttonClass = shelter.website ? "" : "disabled";
         return (
-            
-                <Card className="card">
-                    <div id="color_gradient">Hi</div>
-                    <CardBody>
-                        <CardTitle>{shelter.name}</CardTitle>
-                        <CardSubtitle> {shelter.address} </CardSubtitle>
-                        <CardText> {shelter.phone} </CardText>
-                        <CardText> {shelter.freeBeds} beds available</CardText>
-                        <Button className={buttonClass} color="info" href={shelter.website} target="_blank">Website</Button>
-                    </CardBody>
-                </Card>
+            <Card className="card">
+                <div id="color_gradient">Hi</div>
+                <CardBody>
+                    <CardTitle>{shelter.name}</CardTitle>
+                    <CardSubtitle> {shelter.address} </CardSubtitle>
+                    <CardText> {shelter.phone} </CardText>
+                    <CardText> {shelter.freeBeds} beds available</CardText>
+                    <Button className={buttonClass} color="info" href={shelter.website} target="_blank">Website</Button>
+                </CardBody>
+            </Card>
         );
     }
 }
